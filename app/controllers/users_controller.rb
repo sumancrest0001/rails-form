37f49b15ea2a31270fbd1f92b7f69d75973c57dc
sumancrest0001 <suman.crest0001@gmail.com>
2019-11-16 00:51:18 +0545

    html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user= User.new
   end
-  
+
   # /users
   def create
     @user = User.new(user_params)
-
+    #@user = User.new(name: params[:name], email: params[:email], password: params[:password])
     if @user.save
       redirect_to new_user_path  # goes /users/new
     else
       render :new
     end
   end
-  
-  private 
+
+  private
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
