d3ae920e4e42bfd5aba989ea65c1ccc38ffdb1d2
noomdalv <vladlunasan@gmail.com>
2019-11-15 18:29:05 +0000

    added user form and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new
   end
-
+  
+  # /users
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path  # goes /users/new
+    else
+      render :new
+    end
+  end
+  
+  private 
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
