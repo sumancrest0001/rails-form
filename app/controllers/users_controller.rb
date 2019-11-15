90c5d305c9180d6ca332a402c5308dd64a354068
sumancrest0001 <suman.crest0001@gmail.com>
2019-11-16 04:22:29 +0545

    completed till creating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,21 +2,25 @@ class UsersController < ApplicationController
   def new
     @user= User.new
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
-      redirect_to users_url(@user)
+      redirect_to user_path(@user)
     else
       render 'new'
     end
   end
-  
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def edit
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
 
-  resources :users, only: [:new, :create] 
+  resources :users
 end
